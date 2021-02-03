12acbe27e7881073ab0334527ea4d65c8d5fb721
Vladi Benesova <vladi.benesova1@gmail.com>
2021-02-03 13:16:04 +0100

    form validation

--- src/components/MemberForm.vue
+++ src/components/MemberForm.vue
@@ -1,39 +1,45 @@
 <template>
   <div>
     <form @submit.prevent="submitForm">
-      <div class="form-control">
+      <div class="form-control" :class="{ invalid: !firstName.isValid }">
         <label for="firstname">Firstname</label>
-        <input type="text" id="firstname" v-model.trim="firstName">
+        <input type="text" id="firstname" v-model.trim="firstName.value">
+        <p v-if="!firstName.isValid">Firstname is empty</p>
       </div>
-      <div class="form-control">
+      <div class="form-control" :class="{ invalid: !lastName.isValid }">
         <label for="lastname">Lastname</label>
-        <input type="text" id="lastname" v-model.trim="lastName">
+        <input type="text" id="lastname" v-model.trim="lastName.value">
+        <p v-if="!lastName.isValid">Lastname is empty</p>
       </div>
-      <div class="form-control">
+      <div class="form-control" :class="{ invalid: !location.isValid }">
         <label for="location">Location</label>
-        <input type="text" id="location" v-model.trim="location">
+        <input type="text" id="location" v-model.trim="location.value">
+        <p v-if="!location.isValid">Location is empty</p>
       </div>
-      <div class="form-control">
+      <div class="form-control" :class="{ invalid: !description.isValid }">
         <label for="description">Description</label>
-        <textarea rows="5" id="description" v-model="description"></textarea>
+        <textarea rows="5" id="description" v-model="description.value"></textarea>
+        <p v-if="!description.isValid">Description is empty</p>
       </div>
-      <div class="form-control">
+      <div class="form-control" :class="{ invalid: !areas.isValid }">
         <h3>In which areas you could help other members?</h3>
         <div>
           <span>
             <label for="web">Web-development</label>
-            <input type="checkbox" value="web" id="web" v-model="areas">
+            <input type="checkbox" value="web" id="web" v-model="areas.value">
           </span>
           <span>
             <label for="marketing">Marketing</label>
-            <input type="checkbox" value="marketing" id="marketing" v-model="areas">
+            <input type="checkbox" value="marketing" id="marketing" v-model="areas.value">
           </span>
           <span>
             <label for="sales">Sales</label>
-            <input type="checkbox" value="sales" id="sales" v-model="areas">
+            <input type="checkbox" value="sales" id="sales" v-model="areas.value">
           </span>
         </div>
+        <p v-if="!areas.isValid">Please choose at least one area of your experties</p>
       </div>
+      <p v-if="!formIsValid">Please check your invalid inputs and submit form again</p>
       <base-button>Register</base-button>
     </form>
   </div>
@@ -43,15 +49,58 @@ export default {
   emits:['save-data'],
   data(){
     return {
-      firstName: '',
-      lastName: '',
-      location: '',
-      description: '',
-      areas: []
+      firstName: {
+        value: '',
+        isValid: true
+      },
+      lastName: {
+        value: '',
+        isValid: true
+      },
+      location: {
+        value: '',
+        isValid: true
+      },
+      description: {
+        value: '',
+        isValid: true
+      },
+      areas: {
+        value: [],
+        isValid: true
+      },
+      formIsValid: true
     }
   },
   methods: {
+    validateForm() {
+      this.formIsValid = true;
+      if(this.firstName.value === '') {
+        this.firstName.isValid = false;
+        this.formIsValid = false;
+      };
+      if(this.lastName.value === '') {
+        this.lastName.isValid = false;
+        this.formIsValid = false;
+      };
+      if(this.location.value === '') {
+        this.location.isValid = false;
+        this.formIsValid = false;
+      };
+      if(this.description.value === '') {
+        this.description.isValid = false;
+        this.formIsValid = false;
+      };
+      if(this.areas.value.length === 0) {
+        this.areas.isValid = false;
+        this.formIsValid = false;
+      };
+    },
     submitForm () {
+      this.validateForm();
+      if(!this.formIsValid){
+        return;
+      }
       const formData = {
         first: this.firstName,
         last: this.lastName,
